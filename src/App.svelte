6ab1285d1363f8b80e11cf0be5f71883b47eeb4d
kaanic <44732587+kaanic@users.noreply.github.com>
2026-01-24 22:57:51 +0200

    header base structure

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,10 @@
 <script>
+    import Header from "./lib/Header.svelte";
+    import Main from "./lib/Main.svelte";
+    import Stats from "./lib/Stats.svelte";
+    import Skills from "./lib/Skills.svelte";
+    import Projects from "./lib/Projects.svelte";
+    import Contact from "./lib/Contact.svelte";
 
 </script>
 
@@ -6,6 +12,9 @@
 
 </style>
 
-<html lang="en">
-
-</html>
+<Header />
+<Main />
+<Stats />
+<Skills />
+<Projects />
+<Contact />
---
+++ src/lib/Header.svelte
@@ -0,0 +1,14 @@
+<header>
+    <a href="kaanic.dev" class="logo">
+        > kaanic.dev_
+    </a>
+    <nav>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
+    </nav>
+    <button class="theme-toggle" aria-label="Toggle theme">
+        <!-- icon here later -->
+    </button>
+</header>
