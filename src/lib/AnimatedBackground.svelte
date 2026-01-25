7b6e6f1ccf57fc3bfa3224c61fc12166cfdfcfc8
kaanic <44732587+kaanic@users.noreply.github.com>
2026-01-25 16:24:29 +0200

    floating pixel effect on background

--- src/lib/AnimatedBackground.svelte
+++ src/lib/AnimatedBackground.svelte
@@ -1,12 +1,45 @@
 <script>
+    // instantiating pixels
+    const pixelCount = 20;
+    const pixels = Array.from({length: pixelCount}, (_, i) => ({
+        id: i,
 
+        // left and top will take percentage value
+        left: Math.random() * 100,
+        top: Math.random() * 100,
+
+        delay: Math.random() * 5,
+        duration: 5 + Math.random() * 10,
+
+        colorIndex: i % 3
+    }));
+
+    // getting css variable directly
+    // https://stackoverflow.com/questions/41725725/access-css-variable-from-javascript
+    const root = getComputedStyle(document.documentElement)
+    let colors = [
+        root.getPropertyValue("--accent1").trim(),
+        root.getPropertyValue("--accent2").trim(),
+        root.getPropertyValue("--accent3").trim()
+    ];
 </script>
 
 <div class="animated-bg">
     <div class="grid-bg"></div>
     <div class="scanline-bg"></div>
     <div class="gradient-effect-bg"></div>
-    
+    <div class="pixels-bg">
+        {#each pixels as pixel (pixel.id)}
+            <div class="pixel" style="
+                background-color: {colors[pixel.colorIndex]};
+                left: {pixel.left}%;
+                top: {pixel.top}%;
+                animation-delay: {pixel.delay}s;
+                animation-duration: {pixel.duration}s;
+            "
+            ></div>
+        {/each}
+    </div>
 </div>
 
 <style>
@@ -101,12 +134,54 @@
             );
     }
 
+    .pixels-bg {
+        position: absolute;
+
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+
+        opacity: var(--bgPixelOpacity);
+    }
+
+    .pixel {
+        position: absolute;
+        
+        width: var(--bgPixelSize);
+        height: var(--bgPixelSize);
+
+        opacity: var(--bgPixelOpacity);
+
+        animation: float ease-in-out infinite;
+    }
+
     @keyframes scanline {
         0% {
             transform: translateY(0);
         }
         100% {
             transform: translateY(4px);
+        }
+    }
+
+    @keyframes float {
+        /* 
+            a geometrically correct square is created
+            using 6-8-10 right triangle 4 times per sprint.
+        */
+
+        0%, 100% {
+            transform: translate(0px, 0px) rotate(0deg);
+        }
+        25% {
+            transform: translate(6px, 8px) rotate(90deg);
+        }
+        50% {
+            transform: translate(14px, 2px) rotate(180deg);
+        }
+        75% {
+            transform: translate(8px, -6px) rotate(270deg);
         }
     }
 </style>
