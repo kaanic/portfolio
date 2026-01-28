61ca849b88630053d8755639e3a4d2d75baa4900
kaanic <44732587+kaanic@users.noreply.github.com>
2026-01-28 22:02:43 +0200

    style: complete styling on Projects component

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -20,9 +20,42 @@
     </div>
 
     <div class="show-more">
-        <div class="show-more-text">
+        <div class="show-more-text"> <!-- change to button later on -->
             <!-- https://stackoverflow.com/questions/7381974/which-characters-need-to-be-escaped-in-html -->
             &gt;&gt; SHOW MORE PROJECTS &lt;&lt;
         </div>
     </div>
 </div>
+
+<style>
+    .projects-container {
+        padding: 4rem 0;
+    }
+
+    .projects-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(480px, 1fr));
+
+        gap: 2rem;
+        margin-bottom: 2rem;
+    }
+
+    .show-more {
+        text-align: center;
+
+        margin-top: 3rem;
+    }
+
+    .show-more-text {
+        display: inline-block;
+
+        padding: 1rem 1.5rem;
+        border: 4px solid var(--accent1);
+        box-shadow: 6px 6px color-mix(in srgb, var(--accent1), transparent 50%);
+        
+        background-color: var(--bg);
+        color: var(--accent1);
+
+        font-size: .75rem;
+    }
+</style>
