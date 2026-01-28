da3e26978015c35884dbe1e0920a35c1fc2cee77
kaanic <44732587+kaanic@users.noreply.github.com>
2026-01-28 22:50:28 +0200

    feat: loop through project items on Projects component

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,22 +1,45 @@
 <script>
     import ProjectCard from "./components/ProjectCard.svelte";
 
-    // test
-    const project = {
-            title: 'GAME ENGINE',
+    const projects = [
+        {
+            title: 'E-COMMERCE PLATFORM',
+            desc: 'Full-stack online store with payment integration',
+            tech: ['React', 'Node.js', 'MongoDB'],
+            status: 'COMPLETED',
+            image: 'https://images.unsplash.com/photo-1658297063569-162817482fb6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=800'
+        },
+        {
+            title: 'HGA DISCORD BOT',
+            desc: 'A Discord bot dedicated to HAMK Gaming Academy',
+            tech: ['JavaScript', 'Node.js', 'discord.js'],
+            status: 'COMPLETED',
+            image: 'https://netcord.dev/images/making-a-bot_AddBotToServer_1.webp'
+        },
+        {
+            title: 'DUNGEON ROGUELITE 2D',
+            desc: 'A roguelite game that includes procedural generation',
+            tech: ['Godot', 'GDScript'],
+            status: 'COMPLETED',
+            image: 'https://blog-uploads.eneba.games/uploads/2025/08/deep-rock-galactic-survivor-horde.jpg'
+        },
+        {
+            title: 'CUSTOM GAME ENGINE',
             desc: 'Custom 2D game engine built from scratch',
             tech: ['C++', 'OpenGL', 'SDL'],
             status: 'IN PROGRESS',
-            image: 'https://opengl.bassi.li/devlog/lesson-6-shading.png'
+            image: 'https://preshing.com/images/gameengine-step4.png'
         }
+    ];
 </script>
 
 <div class="projects-container section">
     <h1 class="h1-title"><span class="title-arrow">>></span> PROJECTS.DAT</h1>
 
     <div class="projects-grid">
-        <!-- to be looped -->
-         <ProjectCard {project}/>
+        {#each projects as project}
+            <ProjectCard {project}/>
+        {/each}
     </div>
 
     <div class="show-more">
@@ -52,7 +75,7 @@
         padding: 1rem 1.5rem;
         border: 4px solid var(--accent1);
         box-shadow: 6px 6px color-mix(in srgb, var(--accent1), transparent 50%);
-        
+
         background-color: var(--bg);
         color: var(--accent1);
 
