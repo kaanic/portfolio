4653c5c58e2305aabc0fa34f6e3986f447e09e59
kaanic <44732587+kaanic@users.noreply.github.com>
2026-01-29 14:33:46 +0200

    feat: add Contact w/ ContactItem component

---
+++ src/lib/Contact.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { Github, Mail, Linkedin } from "lucide-svelte";
+    import ContactItem from "./components/ContactItem.svelte";
+</script>
+
+<div class="contact-container section">
+    <h1 class="h1-title"><span class="title-arrow">>></span> CONTACT.INI</h1>
+
+    <div class="contact-content">
+        <p class="font-mono">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta enim dolor, quae at doloribus.</p>
+
+        <div class="contact-items">
+            <ContactItem Icon={Mail} type={"EMAIL"} address={"kaanic1337@gmail.com"}/>
+            <ContactItem Icon={Linkedin} type={"LINKEDIN"} address={"linked.in/kaanic"}/>
+            <ContactItem Icon={Github} type={"GITHUB"} address={"github.com/kaanic"}/>
+        </div>
+    </div>
+</div>
+
+<style>
+    .contact-items {
+        display: flex;
+        justify-content: space-between;
+        flex-direction: row;
+
+        gap: 1rem;
+        padding: 1rem 0;
+    }
+
+    .contact-content {
+        background-color: var(--cardBg);
+        padding: 1.5rem 1.5rem .5rem;
+
+        border: 5px solid var(--border);
+        box-shadow: 8px 8px color-mix(in srgb, var(--borderAccent), transparent 80%);
+    }
+
+    .contact-content > p {
+        font-size: .8rem;
+    }
+</style>
---
+++ src/lib/components/ContactItem.svelte
@@ -0,0 +1,59 @@
+<script>
+    import { Proportions } from "lucide-svelte";
+
+    let { Icon, type, address } = $props();
+</script>
+
+<div class="contact-item-container">
+    <div class="contact-type-icon">
+        <Icon size={24} />
+    </div>
+    <div class="contact-text">
+        <span class="contact-type">{type}</span>
+        <span clasS="contact-address">{address}</span>
+    </div>
+</div>
+
+<style>
+    .contact-item-container {
+        display: flex;
+        flex-direction: row;
+
+        background-color: black; /* change later */
+        border: 4px solid var(--accent2);
+
+        padding: .75rem .25rem;
+        flex: 1;
+    }
+
+    .contact-type-icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        width: 48px;
+    }
+
+    .contact-type-icon {
+        color: var(--accent2);
+    }
+
+    .contact-text {
+        display: flex;
+        flex-direction: column;
+
+        color: white;
+        white-space: nowrap;
+    }
+
+    .contact-address {
+        font-size: .65rem;
+        color: var(--text);
+    }
+    
+    .contact-type {
+        font-size: .75rem;
+        color: var(--textMuted);
+    }
+</style>
+
