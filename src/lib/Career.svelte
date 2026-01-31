c7b06e635ce75f51572750479db136dc14187c3a
kaanic <44732587+kaanic@users.noreply.github.com>
2026-01-31 12:36:57 +0200

    refactor: create js modules for data

--- src/lib/Career.svelte
+++ src/lib/Career.svelte
@@ -1,32 +1,6 @@
 <script>
     import JobLog from "./components/JobLog.svelte";
-
-    const experiences = [
-        {
-            level: 'LVL 2',
-            role: 'Student Assistant',
-            company: 'Hämeen Ammattikorkeakoulu',
-            period: '2024 - PRESENT',
-            status: 'ACTIVE',
-            achievements: [
-                'Guiding students through their studies',
-                'Organizing sessions and assisting teachers',
-                'Also served as team leader fall semester'
-            ]
-        },
-        {
-            level: 'LVL 1',
-            role: 'Digital Image Technician',
-            company: 'Local company',
-            period: '2023 - 2024',
-            status: 'COMPLETED',
-            achievements: [
-                'Created temporary color grades on set',
-                'Maintained and stored footage backups',
-                'Configured camera settings if required'
-            ]
-        }
-    ];
+    import { experiences } from "$lib/data";
 </script>
 
 <div class="career-container section">
---
+++ src/lib/data/experiences.js
@@ -0,0 +1,26 @@
+export const experiences = [
+    {
+        level: 'LVL 2',
+        role: 'Student Assistant',
+        company: 'Hämeen Ammattikorkeakoulu',
+        period: '2024 - PRESENT',
+        status: 'ACTIVE',
+        achievements: [
+            'Guiding students through their studies',
+            'Organizing sessions and assisting teachers',
+            'Also served as team leader fall semester'
+        ]
+    },
+    {
+        level: 'LVL 1',
+        role: 'Digital Image Technician',
+        company: 'Local company',
+        period: '2023 - 2024',
+        status: 'COMPLETED',
+        achievements: [
+            'Created temporary color grades on set',
+            'Maintained and stored footage backups',
+            'Configured camera settings if required'
+        ]
+    }
+];
