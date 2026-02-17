8934653c7435a3e994bc4ce452a408e57d8040bb
kaanic <44732587+kaanic@users.noreply.github.com>
2026-02-17 23:32:00 +0200

    perf: remove render-blocking CSS via JS injection

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), cssInjectedByJsPlugin()],
   base: '/',
   resolve: {
     alias: {
