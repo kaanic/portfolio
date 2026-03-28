749b4320a5c715e9d45ca0f18f16e40997bcdc16
kaanic <44732587+kaanic@users.noreply.github.com>
2026-03-28 12:19:41 +0200

    fix: correct webServer command in playwright

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'npm run start',
+    command: 'npm run preview',
     url: 'http://localhost:4173',
     reuseExistingServer: !process.env.CI,
   },
