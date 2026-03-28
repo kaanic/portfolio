c7c4a6f678065fccdd19c9a234963d8b4086e54b
kaanic <44732587+kaanic@users.noreply.github.com>
2026-03-28 12:00:32 +0200

    test: add SkillCard a11y test

---
+++ src/tests/SkillCard.test.js
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/svelte';
+import { axe } from 'vitest-axe';
+import SkillCard from '$lib/components/SkillCard.svelte';
+
+
+const mock = {
+    name: 'minecraft',
+    proficiency: 'advanced'
+};
+
+describe('SkillCard a11y', () => {
+    it('has no a11y violations', async () => {
+        const { container } = render(SkillCard, {
+            props: { skill: mock }
+        });
+
+        const result = await axe(container);
+        expect(result.violations).toEqual([]);
+    });
+
+    it('has no a11y violations for each proficiency', async () => {
+        for (const proficiency of ['expert', 'advanced', 'intermediate', 'beginner']) {
+            const { container } = render(SkillCard, {
+                props: { skill: {...mock, prof: proficiency} }
+            });
+
+            const result = await axe(container);
+            expect(result.violations).toEqual([]);
+        }
+    });
+})
